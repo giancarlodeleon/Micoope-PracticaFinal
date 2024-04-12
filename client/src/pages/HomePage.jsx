e9fe21fe4eb1177feb9c0825a6997fd7497ec4b2
Giancarlo De Leon <60676409+giancarlodeleon@users.noreply.github.com>
2024-04-12 11:13:43 -0600

    Nuevo Home Page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,42 +1,110 @@
-import logo1 from "../assets/logo1.png";
-
+import { useState } from "react";
+import COPE from "../assets/COPE.jpg";
+import mimarket from "../assets/minimarket.jpg";
+import video from "../assets/video.mp4";
 
 function HomePage() {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpansion = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div className="bg-cover bg-center   ">
-      
-        <div className="flex flex-row justify-center w-full">
-          <div className="flex flex-col items-start w-1/2">
-            <h1 className="text-3xl text-center text-gray-800 mb-6 font-semibold" style={{ marginLeft: '150px', marginTop:'150px' }} >
-              Bienvenido a nuestra cooperativa
-            </h1>
-            <p className="text-lg text-gray-700 mb-6"style={{ marginLeft: '80px', marginTop:'20px' }}>
-              Somos una cooperativa comprometida con el bienestar de nuestros
-              miembros y la comunidad. Trabajamos juntos para alcanzar objetivos
-              comunes y promover el desarrollo sostenible.
-            </p>
+    <div className=" text-white flex flex-col justify-center items-center overflow-hidden">
+      {/* Video de fondo */}
+      <div className="absolute inset-0 z-0 overflow-hidden">
+        <video
+          autoPlay
+          muted
+          loop
+          disablePictureInPicture
+          className="absolute opacity-60"
+          style={{ objectFit: "cover", width: "100%", height: "100%" }}
+        >
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
+
+      {/* Contenido */}
+      <div
+        className="z-10 w-3/4  font-semibold text-borde text-blue-950"
+        style={{ fontFamily: "Century Gothic", fontSize: "80px" }}
+      >
+        <h1 className="text-3xl  mb-6 font-semibold"></h1>
+        <h1
+          className="text-3xl mb-6 font-semibold"
+          style={{ textShadow: "0 0 5px white" }}
+        >
+          COOPERATIVA COBAN
+        </h1>
+        <p className="text-lg text-left mb-6">
+          ofrecemos una plataforma segura y eficiente para el seguimiento de
+          activos y productos. Los usuarios deben registrarse para acceder,
+          garantizando la privacidad y seguridad de los datos. Permite gestionar
+          inventarios, realizar seguimiento de entradas y salidas, así como
+          generar informes detallados sobre el estado de los recursos. Facilita
+          la navegación y el manejo de la información, optimizando la gestión de
+          inventarios y promoviendo una administración transparente y efectiva.
+          Con este sistema, la cooperativa puede mantener un control preciso de
+          sus recursos y mejorar su eficiencia operativa.
+        </p>
+
+        {/* Contenido */}
+        <div className="z-10 w-3/4 text-center font-semibold text-borde text-blue-950">
+          <h1 className="text-3xl text-center mb-6 font-semibold"></h1>
+          <p className="text-lg text-center mb-6"></p>
+        </div>
+      </div>
+
+      {/* Nuevo contenido sin video de fondo */}
+      <div
+        className="z-10 text-center mt-8 bg-white w-full"
+        style={{ fontFamily: "Century Gothic", fontSize: "80px" }}
+      >
+        <h2 className="text-2xl text-gray-800 mb-6">
+          {/* Contenido del encabezado si lo hay */}
+        </h2>
+
+        <p className="text-lg text-gray-700  mb-6">
+          ¡ CAMINAMOS JUNTOS PARA MEJORAR LA CALIDAD DE VIDA DE LOS ASOCIADOS !
+        </p>
+
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {/* Contenedor para la primera imagen */}
+          <div style={{ marginRight: "80px" }}>
+            <img src={COPE} alt="Visión" style={{ width: "90%" }} />
           </div>
-          <div className="mb-6 ml-6 w-1/2">
-            <img
-              src={logo1}
-              alt="Imagen Cooperativa"
-              className="rounded-lg"
-              style={{ width: "500px", height: "auto" }}
-            />
+          {/* Contenedor para la segunda imagen */}
+          <div>
+            <img src={mimarket} alt="Segunda imagen" style={{ width: "90%" }} />
           </div>
         </div>
-        
+
+        {/* Contenido */}
+        <div className="z-10 w-3/4 text-center font-semibold text-borde text-blue-950">
+          <h1 className="text-3xl text-center mb-6 font-semibold"></h1>
+          <p className="text-lg text-center mb-6"></p>
+        </div>
+
         {/* Nuevo apartado centrado horizontal y verticalmente */}
-        <div className="mt-12 flex flex-col items-center justify-center">
-          <h2 className="text-2xl text-center text-gray-800 mb-6">
-            Sobre mí
+        <div
+          className="text-container bg-gray-200 rounded-lg p-6 text-center z-10"
+          style={{
+            marginLeft: expanded ? "-30%" : "0",
+            transition: "margin-left 2s ease",
+          }}
+          onMouseEnter={toggleExpansion}
+          onMouseLeave={toggleExpansion}
+        >
+          <h2 className="text-2xl text-gray-800 mb-6">
+            {expanded ? "Nosotros" : "Contactanos"}
           </h2>
-          <p className="text-lg text-gray-700 mb-6 text-center">
-            Aquí puedes agregar información sobre ti, tus roles en la cooperativa,
-            tu experiencia, tus objetivos, etc.
+          <p className="text-lg text-gray-700 mb-6">
+            {expanded ? "Somos Cooperativa Cobán" : "PBX 7929-7600 "}
           </p>
         </div>
-      
+      </div>
     </div>
   );
 }
