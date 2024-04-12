156e36f612698beb988bf9db5a936d695b50646d
Giancarlo De Leon <60676409+giancarlodeleon@users.noreply.github.com>
2024-04-12 09:23:29 -0600

    .

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,7 +4,6 @@ import { TOKEN_SECRET } from '../config.js';
 export const authRequired = (req,res,next) => {
 
     const {token} = req.cookies;
-    if(!token) return res.status(401).json({message: "No token, autorizacion denegada"})
 
     jwt.verify(token,TOKEN_SECRET,(err,user) => {
         if(err) return res.status(401).json({message: "token invalido"});
